f7ea36b9ea166ee74e0f5a9ca50700b5f1c4a1f4
John Lorenzini <lorenzinijd@gmail.com>
2022-06-26 23:36:20 -0500

    responsive design updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HeroSection from './components/HeroSection'
 
 function App() {
-  return (
-    <HeroSection></HeroSection>
-  )
+  	const [theme, setTheme] = useState(null);
+	
+	function changeTheme(){
+        if(theme === "light"){
+			localStorage.theme = 'dark'
+            setTheme("dark");
+        }
+        else{
+			localStorage.theme = 'light'
+            setTheme("light");
+        }
+    }
+
+	
+	useEffect(() => {
+		if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	}, []);
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}, [theme]);
+
+	return (
+		<body className="font-scp bg-zinc-100 dark:bg-zinc-700 transition-colors duration-300">
+			<button 
+			type='button' 
+			onClick={changeTheme}
+			className = "transition ease-in-out duration-300 material-icons fixed z-10 right-3 top-3  dark:text-zinc-600 text-zinc-50 bg-gradient-to-t from-emerald-400 to-green-400 text-2xl p-2 py-1 rounded-md shadow-xl hover:animate-pulse hover:scale-110"
+			>{theme==='light' ? 'light_mode' : 'dark_mode'}</button>
+				<HeroSection></HeroSection>
+		</body>
+	)
 }
 
 export default App
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div className='flex items-end justify-center flex-col py-20 pt-40 pr-20'>
-        <h1 className='text-6xl md:text-8xl mb-1 md:mb-3 text-emerald-400 font-bold font-scp'>john</h1>
-        <h1 className='text-6xl md:text-8xl mb-1 md:mb-3 text-green-400 font-bold font-scp'>lorenzini</h1>
+    <div className='flex flex-col md:flex-row justify-evenly py-20 pt-40'>
+      <div id="image" className='basis-1/2 items-center'>
+        <a>
+          <img src="../refs/dond.jpg"></img>
+        </a>
+      </div>
+      <div id="text" className='basis-1/4 items-end'>
+        <h1 className='text-right transition ease-in-out text-6xl md:text-8xl mb-1 md:mb-3 text-green-400 dark:text-emerald-400 font-bold hover:scale-110'>john</h1>
+        <h1 className='text-right transition ease-in-out text-6xl md:text-8xl mb-1 md:mb-3 text-emerald-400 dark:text-green-400 font-bold hover:scale-110'>lorenzini</h1>
+        <p className='text-right text-emerald-600 dark:text-green-300 text-sm md:text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis modi vero distinctio commodi sint hic eligendi ut reprehenderit explicabo. Facilis accusamus blanditiis perspiciatis voluptas dignissimos molestias magnam numquam exercitationem. Commodi.</p>
+      </div>
     </div>
   )
 }
