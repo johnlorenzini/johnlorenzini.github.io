e281b3c65357899765f83f2eed6487b95804f8f9
john lorenzini <lorenzinijd@gmail.com>
2022-07-25 11:34:10 -0500

    resized nav

--- src/components/NavDown.jsx
+++ src/components/NavDown.jsx
@@ -7,7 +7,7 @@ function NavDown({ target, delay, anchor, refs }) {
     <div className='navPad self-center'>
       <div data-aos="fade-up" data-aos-anchor={anchor} data-aos-easing="ease" data-aos-duration="800" data-aos-delay={delay} className=''>
         <a href={target} className="text-center">
-          <h1 className='text-center text-3xl py-2 font-medium drop-shadow-md text-zinc-900 dark:text-white'>{refs}</h1>
+          <h1 className='text-center text-2xl py-2 font-medium drop-shadow-md text-zinc-900 dark:text-white'>{refs}</h1>
           <FaChevronDown className='mx-20 transition mb-20 md:mb-0 ease-in-out duration-300 w-8 h-8 text-zinc-900 dark:text-white drop-shadow-md animate-bounce' />
         </a>
       </div>
