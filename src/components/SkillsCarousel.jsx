8cb6cf6042a9a0057c1d0548c5aba1c2b26021a4
john lorenzini <lorenzinijd@gmail.com>
2022-07-25 11:31:21 -0500

    anchor fix

--- src/components/SkillsCarousel.jsx
+++ src/components/SkillsCarousel.jsx
@@ -22,10 +22,10 @@ import {
 function SkillsCarousel() {
   return (
     <div id="skills" className='w-2/3 flex flex-col md:grid md:gap-x-10 md:grid-rows-2 md:grid-cols-4 justify-center'>
-        <div id="skillgrid" data-aos="fade-up" data-aos-easing="ease" data-aos-duration="800" data-aos-anchor="#skills" className='transition ease-in-out my-auto duration-300 mb-6 mt-2 col-start-2 col-span-2 row-start-1 row-span-1 cardBG shadow-xl border-2 border-white border-opacity-10 rounded-2xl'>
+        <div id="dev" data-aos="fade-up" data-aos-easing="ease" data-aos-duration="800" data-aos-anchor="#skills" className='transition ease-in-out my-auto duration-300 mb-6 mt-2 col-start-2 col-span-2 row-start-1 row-span-1 cardBG shadow-xl border-2 border-white border-opacity-10 rounded-2xl'>
             <h1 className='transition ease-in-out duration-500 text-center text-zinc-900 dark:text-white text-3xl md:text-4xl font-semibold pt-4'>code.</h1>
             <hr className='transition ease-in-out duration-500 rounded-xl border-2 my-4 border-zinc-900 dark:border-white'/>
-            <div id="dev" className="mx-auto transition ease-in-out duration-500 text-4xl md:text-5xl grid grid-cols-3 grid-rows-3 justify-items-center gap-2 md:gap-8 text-rose-500 dark:text-rose-400">
+            <div id="skillgrid" className="mx-auto transition ease-in-out duration-500 text-4xl md:text-5xl grid grid-cols-3 grid-rows-3 justify-items-center gap-2 md:gap-8 text-rose-500 dark:text-rose-400">
               <SiPython id="python" />
               <SiJava id="java" />
               <SiCplusplus id="c++" />
@@ -39,10 +39,10 @@ function SkillsCarousel() {
             <div id="title" className="mt-10 font-medium justify-center">
             </div>            
         </div>
-        <div id="skillgrid" data-aos="fade-up" data-aos-easing="ease" data-aos-duration="800" data-aos-anchor="#create" className='transition my-auto ease-in-out duration-300 mb-6 col-start-1 col-span-2 row-start-2 row-span-1 cardBG shadow-xl border-2 border-white border-opacity-10 rounded-2xl'>
+        <div id="tech" data-aos="fade-up" data-aos-easing="ease" data-aos-duration="800" data-aos-anchor="#dev" className='transition my-auto ease-in-out duration-300 mb-6 col-start-1 col-span-2 row-start-2 row-span-1 cardBG shadow-xl border-2 border-white border-opacity-10 rounded-2xl'>
             <h1 className='transition ease-in-out duration-500 text-center text-zinc-900 dark:text-white text-3xl md:text-4xl font-semibold pt-4'>tech.</h1>
             <hr className='transition ease-in-out duration-500 rounded-xl border-2 my-4 border-zinc-900 dark:border-white'/>
-            <div id="tech" className="mx-auto transition ease-in-out duration-500 text-4xl md:text-5xl grid grid-cols-3 grid-rows-3 mb-4 justify-items-center gap-4 md:gap-8 text-rose-500 dark:text-rose-400">
+            <div id="skillgrid" className="mx-auto transition ease-in-out duration-500 text-4xl md:text-5xl grid grid-cols-3 grid-rows-3 mb-4 justify-items-center gap-4 md:gap-8 text-rose-500 dark:text-rose-400">
               <SiGit id="git" />
               <SiJupyter id="jupyter" />
               <SiFirebase id="firebase" />
@@ -53,10 +53,10 @@ function SkillsCarousel() {
             <div id="title" className="mt-10 font-medium justify-center">              
             </div>
         </div>
-        <div id="skillgrid" data-aos="fade-up" data-aos-easing="ease" data-aos-duration="800" data-aos-anchor="#tech" className='transition my-auto ease-in-out duration-300 mb-6 col-start-3 col-span-2 row-start-2 row-span-1 cardBG shadow-xl border-2 border-white border-opacity-10 rounded-2xl'>
+        <div id="creative" data-aos="fade-up" data-aos-easing="ease" data-aos-duration="800" data-aos-anchor="#dev" className='transition my-auto ease-in-out duration-300 mb-6 col-start-3 col-span-2 row-start-2 row-span-1 cardBG shadow-xl border-2 border-white border-opacity-10 rounded-2xl'>
             <h1 className='transition ease-in-out duration-500 text-center text-zinc-900 dark:text-white text-3xl md:text-4xl font-semibold pt-4'>creative.</h1>
             <hr className='transition ease-in-out duration-500 rounded-xl border-2 my-4 border-zinc-900 dark:border-white'/>
-            <div id="creative" className="mx-auto transition ease-in-out duration-500 text-4xl md:text-5xl grid grid-cols-3 grid-rows-3 mb-4 justify-items-center gap-4 md:gap-8 text-rose-500 dark:text-rose-400">
+            <div id="skillgrid" className="mx-auto transition ease-in-out duration-500 text-4xl md:text-5xl grid grid-cols-3 grid-rows-3 mb-4 justify-items-center gap-4 md:gap-8 text-rose-500 dark:text-rose-400">
               <SiFigma />
               <SiAdobephotoshop />
               <SiAdobepremierepro />
