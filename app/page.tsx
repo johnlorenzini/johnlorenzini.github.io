0a9306f216e54fc72be664438e7af523fd4ef38c
john lorenzini <lorenzinijd@gmail.com>
2023-10-06 15:26:51 -0700

    update 10/6

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,71 @@
-import {Anton} from 'next/font/google'
-
-const anton = Anton({weight: '400', subsets: ['latin']})
-
 export default function Home() {
   return (
-    <main className={`${anton.className} bg-[#F6F2F2] text-[#1E1D1E]`}>
-      <div className="grid grid-cols-7 grid-rows-5 h-screen">
-        <div className="row-span-4 row-start-0 items-center flex justify-center LeftToRight "><div className='transform -rotate-90 text-8xl whitespace-nowrap m-0 p-0'>test & test & test</div></div>
-        <div className="col-span-5 row-span-4 row-start-0">
-          <h1 className={`text-[400px]  tracking-widest`}>john</h1>
+    <main className={`h-[400vh] uppercase bg-[#E8E3D0] tracking-tighter`}>
+      <div className="relative h-screen overflow-hidden">
+        <div className="content overflow-x-hidden">
+          <h1 className="test tracking-tight text-[15rem] leading-none text-zinc-800">
+            <span>john</span>
+            <br />
+            <span>lorenzini</span>
+          </h1>
         </div>
-        <div className="row-span-4 col-start-7 row-start-0 items-center flex justify-center RightToLeft"><div className='rotate-90 text-8xl whitespace-nowrap '>nasa & this & that</div></div>
-        <div className="col-span-7 row-start-5 text-8xl overflow-x-hidden"><h1>yes</h1></div>
-
+        <div className="marquee-container invisible md:visible">
+          <div className="left-marquee">
+            <a className="marquee cursor-pointer bg-[#E8E3D0] leading-none">
+              <span className="py-4">
+                data analytics, cloud computing, web development, machine
+                learning,&nbsp;
+              </span>
+            </a>
+            <a className="marquee marquee2 cursor-pointer leading-none">
+              <span className="py-4">
+                data analytics, cloud computing, web development, machine
+                learning,&nbsp;
+              </span>
+            </a>
+          </div>
+          <div className="top-marquee">
+            <a className="marquee cursor-pointer leading-none bg-[#E8E3D0]">
+              <span className="py-4">
+                computer science @ uc irvine | computer science @ uc irvine
+                |&nbsp;
+              </span>
+            </a>
+            <a className="marquee marquee2 cursor-pointer leading-none">
+              <span className="py-4">
+                computer science @ uc irvine | computer science @ uc irvine
+                |&nbsp;
+              </span>
+            </a>
+          </div>
+          <div className="right-marquee">
+            <a className="marquee3 cursor-pointer bg-[#E8E3D0] leading-none">
+              <span className="py-4">
+                nasa, d-bat, springs creative, pepsico,&nbsp;
+              </span>
+            </a>
+            <a className="marquee3 marquee4 cursor-pointer leading-none">
+              <span className="py-4">
+                nasa, d-bat, springs creative, pepsico,&nbsp;
+              </span>
+            </a>
+          </div>
+          <div className="bottom-marquee">
+            <a className="marquee cursor-pointer leading-none bg-[#E8E3D0]">
+              <span className="py-4">
+                computer science & informatics @ uc irvine | computer science &
+                informatics @ uc irvine |&nbsp;
+              </span>
+            </a>
+            <a className="marquee marquee2 cursor-pointer leading-none">
+              <span className="py-4">
+                computer science & informatics @ uc irvine | computer science &
+                informatics @ uc irvine |&nbsp;
+              </span>
+            </a>
+          </div>
+        </div>
       </div>
-      
-    
     </main>
-  )
+  );
 }
