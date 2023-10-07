d7b075aca2a7dcd0dbdeb75eb0a54e8af723c794
john lorenzini <lorenzinijd@gmail.com>
2023-10-06 20:38:34 -0700

    new font

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+import { Playfair_Display } from "next/font/google";
+const Playfair = Playfair_Display({
+  weight: "700",
+  subsets: ["latin-ext"],
+});
+
 export default function Home() {
   return (
     <main className={`h-[400vh] uppercase bg-[#E8E3D0] tracking-tighter`}>
       <div className="relative h-screen overflow-hidden">
         <div className="content overflow-x-hidden">
-          <h1 className="test tracking-tight text-[15rem] leading-none text-zinc-800">
-            <span>john</span>
-            <br />
-            <span>lorenzini</span>
-          </h1>
+          <div className={`${Playfair.className}`}>
+            <h1 className="test tracking-tight text-[15rem] leading-none text-zinc-800">
+              <span>john</span>
+              <br />
+              <span>lorenzini</span>
+            </h1>
+          </div>
         </div>
         <div className="marquee-container invisible md:visible">
           <div className="left-marquee">
