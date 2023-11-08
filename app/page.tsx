512f7a78b4b4eae510170de4108f5d4fb6c43513
john lorenzini <lorenzinijd@gmail.com>
2023-11-07 23:58:30 -0800

    small design update

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ const Playfair = Playfair_Display({
 
 export default function Home() {
   return (
-    <main className={`h-[400vh] uppercase bg-[#E8E3D0] tracking-tighter`}>
+    <main className={`h-[400vh] uppercase bg-zinc-900 tracking-tighter`}>
       <div className="relative h-screen overflow-hidden">
         <div className="content overflow-x-hidden">
           <div className={`${Playfair.className}`}>
-            <h1 className="test tracking-tight text-[15rem] leading-none text-zinc-800">
+            <h1 className="test tracking-tight text-[16rem] pt-40 leading-none text-zinc-800">
               <span>john</span>
               <br />
               <span>lorenzini</span>
@@ -19,56 +19,56 @@ export default function Home() {
         </div>
         <div className="marquee-container invisible md:visible">
           <div className="left-marquee">
-            <a className="marquee cursor-pointer bg-[#E8E3D0] leading-none">
+            <a className="marquee cursor-pointer bg-zinc-900 leading-none">
               <span className="py-4">
-                data analytics, cloud computing, web development, machine
-                learning,&nbsp;
+                data analytics • cloud computing • web development • machine
+                learning •&nbsp;
               </span>
             </a>
             <a className="marquee marquee2 cursor-pointer leading-none">
               <span className="py-4">
-                data analytics, cloud computing, web development, machine
-                learning,&nbsp;
+                data analytics • cloud computing • web development • machine
+                learning •&nbsp;
               </span>
             </a>
           </div>
           <div className="top-marquee">
-            <a className="marquee cursor-pointer leading-none bg-[#E8E3D0]">
+            <a className="marquee cursor-pointer leading-none bg-zinc-900">
               <span className="py-4">
-                computer science @ uc irvine | computer science @ uc irvine
-                |&nbsp;
+                computer science @ uc irvine • computer science @ uc irvine
+                •&nbsp;
               </span>
             </a>
             <a className="marquee marquee2 cursor-pointer leading-none">
               <span className="py-4">
-                computer science @ uc irvine | computer science @ uc irvine
-                |&nbsp;
+                computer science @ uc irvine • computer science @ uc irvine
+                •&nbsp;
               </span>
             </a>
           </div>
           <div className="right-marquee">
-            <a className="marquee3 cursor-pointer bg-[#E8E3D0] leading-none">
+            <a className="marquee3 cursor-pointer bg-zinc-900 leading-none">
               <span className="py-4">
-                nasa, d-bat, springs creative, pepsico,&nbsp;
+                nasa • d-bat • springs creative • pepsico •&nbsp;
               </span>
             </a>
             <a className="marquee3 marquee4 cursor-pointer leading-none">
               <span className="py-4">
-                nasa, d-bat, springs creative, pepsico,&nbsp;
+                nasa • d-bat • springs creative • pepsico •&nbsp;
               </span>
             </a>
           </div>
           <div className="bottom-marquee">
-            <a className="marquee cursor-pointer leading-none bg-[#E8E3D0]">
+            <a className="marquee cursor-pointer leading-none bg-zinc-900">
               <span className="py-4">
-                computer science & informatics @ uc irvine | computer science &
-                informatics @ uc irvine |&nbsp;
+                best overall @ hackuci 2023 • 3rd place @ zothacks 2021 • 3rd
+                place @ webreg hackathon 2023 •&nbsp;
               </span>
             </a>
             <a className="marquee marquee2 cursor-pointer leading-none">
               <span className="py-4">
-                computer science & informatics @ uc irvine | computer science &
-                informatics @ uc irvine |&nbsp;
+                best overall @ hackuci 2023 • board's pick @ uci datathon 2023 •
+                3rd place @ webreg hackathon 2023 •&nbsp;
               </span>
             </a>
           </div>
